afb794c68c25f92c01c4aa7d201a9942e51869d8
Eric Xu <xydxydxyd1@gmail.com>
2025-01-02 15:48:21 -0800

    refactor: add documentation, variables, and remove fluff

--- run.sh
+++ run.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 # run.sh 
-#
 
+lib_path="/usr/lib/jvm/java-18-openjdk-amd64/lib"
+
+# 0. LocalVarRenaming:
+# 1. For2While
+# 2. While2For
+# 3. ReverseIfElse
+# 4. SingleIF2ConditionalExp
+# 5. ConditionalExp2SingleIF
+# 6. PP2AddAssignment
+# 7. AddAssignemnt2EqualAssignment
+# 8. InfixExpressionDividing
+# 9. IfDividing
+# 10. StatementsOrderRearrangement
+# 11. LoopIfContinue2Else
+# 12. VarDeclarationMerging
+# 13. VarDeclarationDividing
+# 14. SwitchEqualSides
+# 15. SwitchStringEqual
+# 16. PrePostFixExpressionDividing
+# 17. Case2IfElse
 test_id=0
-#java -jar ./artifacts/SPAT-linux.jar "$test_id" ./Benchmarks/9133/Original "./Benchmarks/9133/transformed/_$test_id" /usr/lib/jvm/java-8-openjdk/jre/lib
-java -jar ./artifacts/SPAT-linux.jar "$test_id" ./Benchmarks/9133/Original "./Benchmarks/9133/transformed/_$test_id" /usr/lib/jvm/java-21-openjdk/lib
+
+# Assume default directory structure
+# Benchmarks
+# └── "$benchmark_name"
+#     ├── Original
+#     └── transformed
+benchmark_name="Java250"
+
+java -jar ./artifacts/SPAT-linux.jar \
+    "$test_id" \
+    "./Benchmarks/$benchmark_name/Original" \
+    "./Benchmarks/$benchmark_name/transformed/_$test_id" \
+    "$lib_path"
