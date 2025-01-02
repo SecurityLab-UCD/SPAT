ec0cbef6bb064e52c5040675bbf7999a249d0dd8
Eric Xu <xydxydxyd1@gmail.com>
2025-01-02 15:00:11 -0800

    feat: take test_id as argument

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 # run.sh 
 #
 
-jre_path="$(which java)"
-java -jar ./artifacts/SPAT-linux.jar 5 ./Benchmarks/9133/Original ./Benchmarks/9133/transformed/_5 /usr/lib/jvm/java-8-openjdk/jre/lib
+test_id=0
+#java -jar ./artifacts/SPAT-linux.jar "$test_id" ./Benchmarks/9133/Original "./Benchmarks/9133/transformed/_$test_id" /usr/lib/jvm/java-8-openjdk/jre/lib
+java -jar ./artifacts/SPAT-linux.jar "$test_id" ./Benchmarks/9133/Original "./Benchmarks/9133/transformed/_$test_id" /usr/lib/jvm/java-21-openjdk/lib
